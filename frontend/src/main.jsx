3073d691c015fea9c8380840fa826b937c8fb239
Narmadha Priya <48875616+narmadapriya@users.noreply.github.com>
2026-09-23 15:57:23 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
@@ -15,8 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode>
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
